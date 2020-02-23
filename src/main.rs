690329091f0c41a4795c80f92c8fd0898f248487
Giulio De Pasquale <giulio@depasquale.eu>
2020-02-23 13:08:04 +0000

    Removed linux/osx script commands

--- src/main.rs
+++ src/main.rs
@@ -82,7 +82,7 @@ fn main() {
                         .value_name("PUBLIC_KEY")
                         .help(
                             "Use the given public key, and don't generate keys \
-                           automatically",
+                             automatically",
                         ),
                 )
                 .arg(
@@ -118,16 +118,7 @@ fn main() {
                         .help("Name of client to remove"),
                 ),
         )
-        .subcommand(
-            SubCommand::with_name("router-config")
-                .about("Dump router config")
-                .arg(
-                    Arg::with_name("linux-script")
-                        .short("l")
-                        .long("linux-script")
-                        .help("Dump as bash script for linux"),
-                ),
-        )
+        .subcommand(SubCommand::with_name("router-config").about("Dump router config"))
         .subcommand(
             SubCommand::with_name("client-config")
                 .about("Dump client config")
@@ -136,19 +127,6 @@ fn main() {
                         .value_name("NAME")
                         .required(true)
                         .help("Name of client to dump configuration for"),
-                )
-                .arg(
-                    Arg::with_name("linux-script")
-                        .short("l")
-                        .long("linux-script")
-                        .help("Dump as bash script for linux"),
-                )
-                .arg(
-                    Arg::with_name("osx-script")
-                        .short("o")
-                        .long("osx-script")
-                        .conflicts_with("linux-script")
-                        .help("Dump as bash script for Mac OS X"),
                 ),
         )
         .get_matches();
@@ -248,86 +226,23 @@ fn main() {
         configuration.save(Path::new(filename));
 
         println!("Client added");
-    } else if let Some(matches) = matches.subcommand_matches("router-config") {
+    } else if let Some(_) = matches.subcommand_matches("router-config") {
         let configuration = Configuration::open(Path::new(filename));
-
-        if matches.is_present("linux-script") {
-            println!("cat > vpn.conf <<EOF");
-        }
 
         println!("{}", configuration.router().interface());
 
         for client in configuration.clients() {
             println!("{}", client.peer());
         }
-
-        if matches.is_present("linux-script") {
-            println!("EOF");
-            println!("ip link add dev wg0 type wireguard");
-            println!(
-                "ip address add dev wg0 {}/32",
-                configuration.router().internal_address()
-            );
-            println!("wg setconf wg0 vpn.conf");
-            println!("ip link set up dev wg0");
-
-            configuration
-                .clients()
-                .iter()
-                .flat_map(|client| client.allowed_ips())
-                .for_each(|allowed_ip| println!("ip route add {} dev wg0", allowed_ip));
-        }
     } else if let Some(matches) = matches.subcommand_matches("client-config") {
         let configuration = Configuration::open(Path::new(filename));
         let name = matches.value_of("name").unwrap();
 
-        let client = configuration
+        configuration
             .client_by_name(name)
             .expect(&format!("Could not find client {}", name));
 
-        if matches.is_present("linux-script") || matches.is_present("osx-script") {
-            println!("cat > vpn.conf <<EOF");
-        }
-
         println!("{}", configuration.client_config(name).unwrap());
-
-        if matches.is_present("linux-script") {
-            println!("EOF");
-            println!("ip link add dev wg0 type wireguard");
-            println!("ip address add dev wg0 {}/32", client.internal_address());
-            println!("wg setconf wg0 vpn.conf");
-            println!("ip link set up dev wg0");
-
-            println!(
-                "ip route add {} dev wg0",
-                configuration.router().internal_address()
-            );
-
-            configuration
-                .all_allowed_ips()
-                .iter()
-                .for_each(|allowed_ip| println!("ip route add {} dev wg0", allowed_ip));
-        } else if matches.is_present("osx-script") {
-            println!("EOF");
-            println!("sudo wireguard-go utun9");
-            println!("sudo wg setconf utun9 vpn.conf");
-            println!(
-                "sudo ifconfig utun9 inet {} 255.255.255.255 {}",
-                client.internal_address(),
-                configuration.router().internal_address()
-            );
-
-            configuration
-                .all_allowed_ips()
-                .iter()
-                .for_each(|allowed_ip| {
-                    if allowed_ip.prefix_len() == 32 {
-                        println!("sudo route add {} -interface utun9", allowed_ip.addr());
-                    } else {
-                        println!("sudo route add -net {} -interface utun9", allowed_ip);
-                    }
-                });
-        }
     } else {
         eprintln!("No command!");
         exit(1)
